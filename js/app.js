64461a4ca476e486fbd661b316678c9fb96d502c
catcarbonell <hello@catcarbn.com>
2020-02-08 11:37:02 -0800

    Added Player's kaomoji library

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 console.log("ʕノ•ᴥ•ʔノ ︵ ┻━┻  ︵　しʕ•ᴥ•し  ʔ");
+
+const playerKao = [
+    {
+        id: "player-start",
+        status: "neutral",
+        face: "(๑و•̀ω•́)و",
+        value: 0
+    },
+    {
+        id: "player-hit-dmg",
+        status: "hit-dmg",
+        face: "໒(⁄›˅̭‹∖)७",
+        value: 1
+    },
+    {
+        id: "player-throw-dmg",
+        status: "throw-dmg",
+        face: "╭ (o^o#)ᕗ",
+        value: 2
+    },
+    {
+        id: "player-block",
+        status: "block",
+        face: "(`へ´*)ノ||",
+        value: 0
+    },
+    {
+        id: "player-strike",
+        status: "strike-atk",
+        face: "‾͟͟͞(((ꎤ๑>༬>๑)̂—̳͟͞͞o",
+        value: 1
+    },
+    {
+        id: "player-throw",
+        status: "throw-atk",
+        face: "(┛ಠДಠ)┛ミ ",
+        value: 2
+    },
+
+]
+
+console.log(playerKao);
